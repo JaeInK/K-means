e03befe973e52b2919ec79cd8df4a774be7eb436
JaeInK <gjames5809@gmail.com>
2018-04-12 01:22:32 +0900

    Segmentation error unsolved

--- main.cpp
+++ main.cpp
@@ -1,28 +1,47 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
+#include <stdlib.h>
 
 using namespace std;
 
 int main()
 {
-	//read input
-	string filePath="input.txt";
+	//read input		
+	int testnum;
+	int i;
+	int repeatnum;
+	int clusternum;
+	int pointnum;
+	string str;
+	int space;
 	
-	ifstream openFile(filePath.data());
-	
-	if( openFile.is_open() )
-	{
-		string line;
-		int testnum;
-		int i;
-		testnume = getline(openFile,line);
+	cout<<"start";
+	cin >> testnum;
 
-		for(i=0; i<testnum; i++)
+	for(i=0; i<testnum; i++)
+	{	
+		cin >> repeatnum;
+		cin >> clusternum;
+		cin >> pointnum;
+		vector< vector<double> > V(pointnum);
+		vector<double> tmp;
+		for(i=0; i<pointnum; i++)
 		{
-			while(getline(openFile, line)){
-			cout << line << endl;
-		}
-		openFile.close();
+			cout<<"a";
+			cin.clear();
+			getline(cin, str);
+			//space = str.find(" ");
+			space=1;
+			cout<<"b";
+			cout<<str.length();
+			tmp.push_back(atof(str.substr(0, space).c_str()));
+			tmp.push_back(atof(str.substr(space, str.size()-space-1).c_str()));
+			cout<<"c";
+			V.push_back(tmp);
+			for(int i=0; i<V.size(); ++i)
+				  cout << V[i][0] << ' ';
+				  cout << V[i][1] << ' ';
 		}
 	}
 }
