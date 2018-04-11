3db6f368af5644bc4b9c176288e5e88d944acb91
JaeInK <gjames5809@gmail.com>
2018-04-11 09:51:00 +0900

    reading input

--- main.cpp
+++ main.cpp
@@ -5,15 +5,24 @@ using namespace std;
 
 int main()
 {
+	//read input
 	string filePath="input.txt";
 	
 	ifstream openFile(filePath.data());
 	
-	if( openFile.is_open() ){
+	if( openFile.is_open() )
+	{
 		string line;
-		while(getline(openFile, line)){
+		int testnum;
+		int i;
+		testnume = getline(openFile,line);
+
+		for(i=0; i<testnum; i++)
+		{
+			while(getline(openFile, line)){
 			cout << line << endl;
 		}
 		openFile.close();
+		}
 	}
 }
